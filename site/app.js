1e0bc18f640556662366700c1b25eb5db3fa5fb1
Roman Shamov <dreigfire@mail.ru>
2023-09-28 17:16:02 +0300

    Added input field with button, which adds contents

---
+++ site/app.js
@@ -0,0 +1,21 @@
+const list = document.getElementById("ul-main");
+const btn = document.getElementById("add-button");
+const input = document.getElementById("input-main");
+
+btn.addEventListener("click", function (event) {
+  addInputValueToList();
+});
+input.addEventListener("keydown", function (event) {
+  if (event.key === "Enter" && document.activeElement === input) {
+    addInputValueToList();
+  }
+});
+
+function addInputValueToList() {
+  if (input.value == "") return; // input field is empty, action ignored
+  var newElement = document.createElement("li");
+  newElement.classList.add("list-group-item");
+  newElement.appendChild(document.createTextNode(input.value));
+  input.value = "";
+  list.appendChild(newElement);
+}
